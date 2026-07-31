ea53a2d0016ef5bbcd02eb68a92a15dc2a4ebf19
farheen-tahirr <farheentahir730@gmail.com>
2026-07-31 17:25:58 +0500

    Implemented post creation and feed API

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -30,6 +30,30 @@ const createPost = async (req, res) => {
     }
 };
 
+// ======================
+// Get All Posts
+// ======================
+const getAllPosts = async (req, res) => {
+    try {
+
+        const posts = await Post.find()
+            .populate("user", "name university profileImage role")
+            .sort({ createdAt: -1 });
+
+        res.status(200).json(posts);
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            message: "Server Error",
+        });
+
+    }
+};
+
 module.exports = {
     createPost,
+    getAllPosts,
 };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const protect = require("../middleware/authMiddleware");
-const { createPost } = require("../controllers/postController");
+const {
+    createPost,
+    getAllPosts,
+} = require("../controllers/postController");
 
 const router = express.Router();
 
 // Create Post
 router.post("/", protect, createPost);
+
+// Get All Posts
+router.get("/", getAllPosts);
 
 module.exports = router;
